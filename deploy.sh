f5721f6d8dbbc42cacd9e41715bdec0843eda670
BFK <bkgazda@gmail.com>
2024-07-09 23:50:37 +0200

    wip: check php requirements

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,63 @@ set -e
 # Print each command before executing it
 set -x
 
-# Verify Laravel requirments
+# Verify Laravel requirements
+# Check PHP version is at least 8.2
+REQUIRED_PHP_VERSION="8.2"
+PHP_VERSION=$(php -r "echo PHP_VERSION;")
+
+version_compare() {
+    if [[ $1 == $2 ]]
+    then
+        return 0
+    fi
+    local IFS=.
+    local i ver1=($1) ver2=($2)
+    for ((i=${#ver1[@]}; i<${#ver2[@]}; i++))
+    do
+        ver1[i]=0
+    done
+    for ((i=0; i<${#ver1[@]}; i++))
+    do
+        if [[ -z ${ver2[i]} ]]
+        then
+            ver2[i]=0
+        fi
+        if ((10#${ver1[i]} > 10#${ver2[i]}))
+        then
+            return 1
+        fi
+        if ((10#${ver1[i]} < 10#${ver2[i]}))
+        then
+            return 2
+        fi
+    done
+    return 0
+}
+
+# Use the version_compare function
+version_compare "$PHP_VERSION" "$REQUIRED_PHP_VERSION"
+compare_result=$?
+
+if [ $compare_result -eq 1 ] || [ $compare_result -eq 0 ]; then
+    echo "PHP version is $PHP_VERSION - OK (meets or exceeds required version $REQUIRED_PHP_VERSION)"
+else
+    echo "PHP version is $PHP_VERSION - FAIL (requires $REQUIRED_PHP_VERSION or higher)"
+    exit 1
+fi
+
+# Check PHP extensions
+REQUIRED_EXTENSIONS=("ctype" "curl" "dom" "fileinfo" "filter" "hash" "mbstring" "openssl" "pcre" "pdo" "session" "tokenizer" "xml")
+
+for EXTENSION in "${REQUIRED_EXTENSIONS[@]}"; do
+    if php -m | grep -q "$EXTENSION"; then
+        echo "PHP extension $EXTENSION - OK"
+    else
+        echo "PHP extension $EXTENSION - FAIL"
+        exit 1
+    fi
+done
+echo "All requirements are met. You can run Laravel 11 on this server."
 
 # Verify Laravel .env file
 # Define the environment variable names to check
@@ -18,14 +74,20 @@ if [ -f .env ]; then
     for var in "${ENV_VARS[@]}"; do
         # Get the value of the variable from .env file
         value_env=$(grep -E "^$var=" .env | cut -d= -f2)
-        # Get the value of the variable from .env_production file
-        value_prod=$(grep -E "^$var=" .env_production | cut -d= -f2)
         
         # Compare the values
-        if [ "$value_env" = "$value_prod" ]; then
-            echo "$var is set correctly in .env file."
-        else
-            echo "Error: $var is not set correctly in .env file. Expected value: $value_prod"
+        if [[ "$var" == "APP_ENV" ]]; then
+            if [ "$value_env" = "production" ]; then
+                echo "$var is set correctly in .env file."
+            else
+                echo "Error: $var is not set correctly in .env file. Expected value: production"
+            fi
+        elif [[ "$var" == "APP_DEBUG" ]]; then
+            if [ "$value_env" = "false" ]; then
+                echo "$var is set correctly in .env file."
+            else
+                echo "Error: $var is not set correctly in .env file. Expected value: false"
+            fi
         fi
     done
 else
@@ -70,9 +132,10 @@ php artisan view:cache
 
 # Directory permissions
 # Laravel will need to write to the bootstrap/cache and storage directories, so you should ensure the web server process owner has permission to write to these directories.
-
+# Set the permissions of the bootstrap/cache and storage directories to 0777.
 # bootstrap/cache
 # storage
+sudo chmod -R 0777 bootstrap/cache storage
 
 
 
